1238eaa35d53b083df78f0d03774441878eab0b7
Barna Tamas <barnatamas@gmail.com>
2023-10-23 01:52:15 +0200

    add env variables

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,17 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site", {environment: {DATABASE_URL: process.env.DATABASE_URL!}});
+      const site = new NextjsSite(stack, "site", {
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL!,
+          AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID!,
+          AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY!,
+          NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
+          DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID!,
+          DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET!,
+        }
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
