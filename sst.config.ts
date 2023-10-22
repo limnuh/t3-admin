b7cdb7342bf1e89900dc04f19e89559bdbf46a5b
Barna Tamas <barnatamas@gmail.com>
2023-10-22 19:09:48 +0200

    add sst stuff + replace db to supabase

---
+++ sst.config.ts
@@ -0,0 +1,20 @@
+import { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "t3-admin",
+      region: "eu-central-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site", {environment: {DATABASE_URL: process.env.DATABASE_URL || ''}});
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig; 
