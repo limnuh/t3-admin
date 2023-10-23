1a8693e1a60d70dc5d5f588c3c1da84a60e33894
Barna Tamas <barnatamas@gmail.com>
2023-10-23 14:01:00 +0200

    fix: prettier and run all files

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,15 +1,11 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { type GetServerSidePropsContext } from "next";
-import {
-  getServerSession,
-  type DefaultSession,
-  type NextAuthOptions,
-} from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { type GetServerSidePropsContext } from 'next';
+import { getServerSession, type DefaultSession, type NextAuthOptions } from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
+import EmailProvider from 'next-auth/providers/email';
 
-import { env } from "~/env.mjs";
-import { prisma } from "~/server/db";
+import { env } from '~/env.mjs';
+import { prisma } from '~/server/db';
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -17,9 +13,9 @@ import { prisma } from "~/server/db";
  *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session extends DefaultSession {
-    user: DefaultSession["user"] & {
+    user: DefaultSession['user'] & {
       id: string;
       // ...other properties
       // role: UserRole;
@@ -71,8 +67,8 @@ export const authOptions: NextAuthOptions = {
  * @see https://next-auth.js.org/configuration/nextjs
  */
 export const getServerAuthSession = (ctx: {
-  req: GetServerSidePropsContext["req"];
-  res: GetServerSidePropsContext["res"];
+  req: GetServerSidePropsContext['req'];
+  res: GetServerSidePropsContext['res'];
 }) => {
   return getServerSession(ctx.req, ctx.res, authOptions);
 };
--- sst.config.ts
+++ sst.config.ts
@@ -1,23 +1,23 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { SSTConfig } from 'sst';
+import { NextjsSite } from 'sst/constructs';
 
 export default {
   config(_input) {
     return {
-      name: "t3-admin",
-      region: "eu-central-1",
+      name: 't3-admin',
+      region: 'eu-central-1',
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site", {
+      const site = new NextjsSite(stack, 'site', {
         environment: {
           DATABASE_URL: process.env.DATABASE_URL!,
           NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
           NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
           DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID!,
           DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET!,
-        }
+        },
       });
 
       stack.addOutputs({
@@ -25,4 +25,4 @@ export default {
       });
     });
   },
-} satisfies SSTConfig; 
+} satisfies SSTConfig;
