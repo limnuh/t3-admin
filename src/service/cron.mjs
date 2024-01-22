bc644ce57fe60d20f6bb336baa71fe1f793ff561
Barna Tamas <barnatamas@gmail.com>
2024-01-22 13:45:27 +0100

    feat: cron trigger hourly job

--- src/service/cron.mjs
+++ src/service/cron.mjs
@@ -1,13 +1,20 @@
+import axios from 'axios';
 import cron from 'node-cron';
 
-cron.schedule('12 * * * *', () => {
-  console.log('Running a task every hour :12 minute');
+const triggerDataCollect = async () => {
+  await axios.get('http://localhost:3000/api/scraper/collect');
+};
 
+cron.schedule('19 * * * *', () => {
+  console.log('Running a task every hour :19 minute');
+  triggerDataCollect().catch(() => {
+    // do nothing
+  });
   // run through every search, scrape data, save and create history of cars
 });
 
 cron.schedule('0 0 * * *', () => {
-  console.log('Running a task every midnight');
+  console.log('Running a task every midnight (no nothing now)');
 
   // create agregated data about the current search data (without scrape)
 });
